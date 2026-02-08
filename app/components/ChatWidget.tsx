8ef1d75c844f577cae04063402a607ba617916d7
mhitratali-cmd <mhitratali@gmail.com>
2026-02-08 22:03:52 +0500

    updated the chat agent

--- app/components/ChatWidget.tsx
+++ app/components/ChatWidget.tsx
@@ -9,18 +9,34 @@ export default function ChatWidget() {
       <div 
         id="VG_OVERLAY_CONTAINER" 
         style={{ width: 0, height: 0 }} 
-      />
+      >
+        {/* Here is where CONVOCORE renders the widget. */}
+        {/* Set render to 'full-width' then adjust the width and height to 500px (for example) to render the chatbot itself without the popup. */}
+      </div>
       
       {/* Widget Configuration */}
       <Script id="convocore-config" strategy="afterInteractive">
         {`
           window.VG_CONFIG = {
-            ID: "hrOAuMO1MM6Z5bSH8aJS",
-            region: "eu",
-            render: "bottom-right",
+            ID: "hrOAuMO1MM6Z5bSH8aJS", // YOUR AGENT ID
+            region: 'eu', // YOUR ACCOUNT REGION 
+            render: 'bottom-right', // can be 'full-width' or 'bottom-left' or 'bottom-right'
+            // modalMode: true, // Set this to 'true' to open the widget in modal mode
             stylesheets: [
-              "https://vg-bunny-cdn.b-cdn.net/vg_live_build/styles.css"
-            ]
+                // Base CONVOCORE CSS
+                "https://vg-bunny-cdn.b-cdn.net/vg_live_build/styles.css",
+                // Add your custom css stylesheets, Can also add relative URL ('/public/your-file.css)
+            ],
+            // THIS IS SUPPOSED TO BE CHANGED OR REMOVED.
+            // user: {
+            //     name: 'John Doe', // User's name
+            //     email: 'johndoe@gmail.com', // User's email
+            //     phone: '+1234567890', // User's phone number
+            // }
+            // Optional user data -- end
+            // **
+            // userID: 'USER_ID', // If you want to use your own user_id
+            // autostart: true, // Whether to autostart the chatbot with the proactive message
           };
         `}
       </Script>
